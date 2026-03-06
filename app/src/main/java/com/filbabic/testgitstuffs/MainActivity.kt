e526b50e0b0027f0a8e16fc87f0f31c5d4e2dafb
filbabic <fbabic96@gmail.com>
2026-03-06 19:11:38 +0100

    Changing hello to tadej

--- app/src/main/java/com/filbabic/testgitstuffs/MainActivity.kt
+++ app/src/main/java/com/filbabic/testgitstuffs/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             TestGitStuffsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Tadej",
                         modifier = Modifier.padding(innerPadding),
                     )
                 }
