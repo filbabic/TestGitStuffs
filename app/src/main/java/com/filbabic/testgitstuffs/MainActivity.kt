97553e8786ce7f3bbcc3fe061afc0d8f702b7ffa
filbabic <fbabic96@gmail.com>
2026-03-06 19:26:06 +0100

    Making a change to create a conflict

--- app/src/main/java/com/filbabic/testgitstuffs/MainActivity.kt
+++ app/src/main/java/com/filbabic/testgitstuffs/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             TestGitStuffsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Tadej",
+                        name = "Some change",
                         modifier = Modifier.padding(innerPadding),
                     )
                 }
